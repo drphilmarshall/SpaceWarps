5de30d8fa3dc8cd12dc7869de5827ed3dd247389
cpadavis <chris.pa.davis@gmail.com>
2014-07-17 11:52:58 -0700

    and roc_curves

--- analysis/make_roc_curves.py
+++ analysis/make_roc_curves.py
@@ -23,6 +23,7 @@ params = { 'axes.labelsize': bfs,
 plt.rcParams.update(params)
 
 import swap
+from swap.offline import EM_algorithm
 
 # ======================================================================
 
@@ -41,11 +42,12 @@ def make_roc_curves(argv):
         For the offline assessments, see em.pdf in the docs directory.
 
     FLAGS
-        -h                Print this message
-        -o                Do the offline EM processing
+        -h                          Print this message
+        --offline_stage1    0/1     Do offline analysis for stage1
+        --offline_stage2    0/1     Do offline analysis for stage2
+        --save_offline      0/1     Save the results of offline analysis?
 
     INPUTS
-        --offline 0/1
         stage1_bureau.pickle
         stage2_bureau.pickle
         stage1_collection.pickle
@@ -65,27 +67,33 @@ def make_roc_curves(argv):
         http://spacewarps.org/
 
     HISTORY
-      2013-07-01  started Davis (KIPAC)
+        2013-07-01  started Davis (KIPAC)
     """
 
     # ------------------------------------------------------------------
 
     try:
-       opts, args = getopt.getopt(argv,"ho",["help","offline"])
+       opts, args = getopt.getopt(argv,"h",
+               ["help", "offline_stage1", "offline_stage2"])
     except getopt.GetoptError, err:
        print str(err) # will print something like "option -a not recognized"
        print make_roc_curves.__doc__  # will print the big comment above.
        return
 
-    offline = 0
-    resurrect = False
+    offline_stage1 = 0
+    offline_stage2 = 0
+    save_offline = 0
 
     for o,a in opts:
        if o in ("-h", "--help"):
           print make_roc_curves.__doc__
           return
-       elif o in ("-o", "--offline"):
-          offline = int(a)
+       elif o in ("--offline_stage1"):
+          offline_stage1 = int(a)
+       elif o in ("--offline_stage2"):
+          offline_stage2 = int(a)
+       elif o in ("--save_offline"):
+          save_offline = int(a)
        else:
           assert False, "unhandled option"
 
@@ -100,15 +108,15 @@ def make_roc_curves(argv):
         print "make_roc_curves: ",bureau2_path
         print "make_roc_curves: ",collection1_path
         print "make_roc_curves: ",collection2_path
-    else:
+    else:b
         print make_roc_curves.__doc__
         return
 
     output_directory = './'
 
     # ------------------------------------------------------------------
-
     # Read in bureau and collection objects:
+
     bureau1 = swap.read_pickle(bureau1_path, 'bureau')
     collection1 = swap.read_pickle(collection1_path, 'collection')
     bureau2 = swap.read_pickle(bureau2_path, 'bureau')
@@ -118,154 +126,6 @@ def make_roc_curves(argv):
 
     print "make_roc_curves: stage 1, 2 subject numbers: ",len(collection1.list()), len(collection2.list())
 
-
-    # ------------------------------------------------------------------
-    # if we do the EM offline processing for stage2:
-    # NOTE: it is not inconceivable to also do this for stage1...
-    if offline:
-        print "make_roc_curves: doing offline processing!"
-
-        # NOTE: pi is currently NOT USED
-        def Estep(taus, agents, subjects, pi, set_aside={}):
-            # evaluate taus given collection of subjects and current evaluation of agents
-            taus_prime = taus.copy()
-            for ID in taus:
-                if ID in set_aside:
-                    continue
-                pi_ij = taus[ID] #pi
-                subject = subjects.member[ID]
-                tau_j = 0
-                N_j = 0
-                for agent_i in xrange(len(subject.annotationhistory['Name'])):
-                    name = subject.annotationhistory['Name'][agent_i]
-                    agent = agents[name]
-                    xij = agent['Subjects'][ID]
-                    thetai0 = agent['Theta0']
-                    thetai1 = agent['Theta1']
-
-                    # xij = 1 term
-                    pos = thetai1 ** xij * (1 - thetai1) ** (1 - xij) * pi_ij
-                    neg = thetai0 ** (1 - xij) * (1 - thetai0) ** xij * (1 - pi_ij)
-                    tau_j += pos / (pos + neg)
-
-                    # xij = 0 term
-                    N_j += 1
-                tau_j /= N_j
-
-                taus_prime.update({ID: tau_j})
-            return taus_prime
-
-        def Mstep(agents, taus, pi, training_IDs={}, set_aside={}):
-            pi_num = 0
-            pi_den = 0
-            agents_prime = agents.copy()
-            for name in agents:
-                agent = agents[name]
-                agent_prime = agent.copy()
-                thetai1_num = 0
-                thetai1_den = 0
-                thetai0_num = 0
-                thetai0_den = 0
-                # pii_num = thetai1_den
-                pii_den = 0
-                for ID in agent['Subjects']:
-                    if ID in set_aside:
-                        continue
-                    if ID in taus:
-                        tauj = taus[ID]
-
-                        # Incorporate true value of training values
-                        if ID in training_IDs:
-                            tauj = training_IDs[ID]
-                        xij = agent['Subjects'][ID]
-
-                        thetai0_num += (1 - xij) * (1 - tauj)
-                        thetai0_den += 1 - tauj
-                        thetai1_num += xij * tauj
-                        thetai1_den += tauj
-                        pii_den += 1
-                # I think this is OK?
-                if thetai0_den == 0:
-                    thetai0_den = 1
-                if thetai1_den == 0:
-                    thetai1_den = 1
-
-                agent_prime.update({'Theta0': thetai0_num / thetai0_den,
-                                    'Theta1': thetai1_num / thetai1_den,
-                                    'Pi': thetai1_den / pii_den,})
-                agents_prime.update({name: agent_prime})
-                pi_num += thetai1_den
-                pi_den += pii_den
-            pi = pi_num / pi_den
-            return agents_prime, pi
-
-
-        # Set up data for EM algorithm
-
-        agents = {}
-        taus = {}
-        online_taus = {}
-        training_IDs = {}  # which entries in collection are training
-        set_aside = {}  # which entries do we set aside? Here we set aside none
-        n_min = 1  # minimum number of assessments required to be considered
-
-
-        collection = collection2
-        bureau = bureau2
-
-        for ID in collection.list():
-            subject = collection.member[ID]
-            n_assessment = len(subject.annotationhistory['ItWas'])
-            if (n_assessment > n_min):
-                if (subject.category == 'training'):
-                    truth = {'LENS': 1, 'NOT': 0}[subject.truth]
-                    training_IDs.update({ID: truth})
-                taus.update({ID: pi})
-                online_taus.update({ID: subject.mean_probability})
-                for agent_i in xrange(len(subject.annotationhistory['Name'])):
-                    name = subject.annotationhistory['Name'][agent_i]
-                    xij = subject.annotationhistory['ItWas'][agent_i]
-                    if name not in agents:
-                        agents.update({name: {'Theta0': 0.75, 'Theta1': 0.75,
-                                              'PL': bureau.member[name].PL,
-                                              'PD': bureau.member[name].PD,
-                                              'Pi': pi,
-                                              'Subjects': {ID: xij}}})
-                    else:
-                        agents[name]['Subjects'].update({ID: xij})
-
-
-        # Run EM Algorithm
-
-        epsilon_taus = 10
-        N_max = 50
-        N_min = 10
-        N_try = 0
-        epsilon_min = 1e-5
-        epsilon_list = []
-
-        while (epsilon_taus > epsilon_min) * (N_try < N_max) + (N_try < N_min):
-
-            # E step
-            # for each tau_j go through all classifications i done on j: x_ij
-            taus_prime = Estep(taus, agents, collection, pi, set_aside)
-            # evaluate change in probability
-            epsilon_taus = 0
-            for ID in taus:
-                epsilon_taus += np.square(taus[ID] - taus_prime[ID])
-            taus = taus_prime
-
-            # M step
-            agents_prime, pi_prime = Mstep(agents, taus, pi, training_IDs, set_aside)
-            agents = agents_prime
-            pi = pi_prime
-
-            # divide epsilon_taus by the number of taus
-            epsilon_taus = np.sqrt(epsilon_taus) / len(taus.keys())
-            epsilon_list.append(epsilon_taus)
-            N_try += 1
-
-        print "make_roc_curves: offline processing done! epsilon, number of iterations", epsilon_taus, N_try
 
     # ------------------------------------------------------------------
     # set up data for roc plots
@@ -295,8 +155,74 @@ def make_roc_curves(argv):
         tprs.append(tpr)
         thresholds.append(threshold)
 
-    # now do the stage 2 offline if we wish
-    if offline:
+    # ------------------------------------------------------------------
+    # if we do the EM offline processing for stage1:
+    if offline_stage1:
+        print "make_roc_curves: doing offline_stage1 processing!"
+
+        # Set up data for EM algorithm
+
+        bureau_offline = {}
+        pi = 2e-4
+        taus = {}
+        online_taus = {}
+        training_IDs = {}  # which entries in collection are training
+        training_IDs_blank = {}  # we don't train on training_IDs
+        set_aside_subject = {}  # which subjects do we set aside? Here we set aside none
+        set_aside_agent = {}  # which agents do we set aside? Here we set aside none
+        n_min = 1  # minimum number of assessments required to be considered
+
+        collection = {}
+        for ID in collection1.list():
+            if ID in set_aside_subject:
+                continue
+            else:
+                collection.update({ID: collection1.member[ID]})
+        # TODO: maybe repeat the same process for bureau?
+        bureau = bureau1
+
+        for ID in collection.list():
+            subject = collection.member[ID]
+            n_assessment = len(subject.annotationhistory['ItWas'])
+            if (n_assessment > n_min):
+                if (subject.category == 'training'):
+                    truth = {'LENS': 1, 'NOT': 0}[subject.truth]
+                    # NOTE: setting this aside for here.
+                    training_IDs.update({ID: truth})
+                taus.update({ID: pi})
+                online_taus.update({ID: subject.mean_probability})
+                for agent_i in xrange(len(subject.annotationhistory['Name'])):
+                    name = subject.annotationhistory['Name'][agent_i]
+                    if name in set_aside_agent:
+                        continue
+                    xij = subject.annotationhistory['ItWas'][agent_i]
+                    if name not in bureau_offline:
+                        bureau_offline.update({name: {
+                            'Theta0': 0.75, 'Theta1': 0.75,
+                            'PL': bureau.member[name].PL,
+                            'PD': bureau.member[name].PD,
+                            'Pi': pi,
+                            'Subjects': {ID: xij}}})
+                    else:
+                        bureau_offline[name]['Subjects'].update({ID: xij})
+
+        # Run EM Algorithm
+
+        bureau_offline, pi, taus, information_dict = EM_algorithm(
+                bureau_offline, pi, taus, training_IDs_blank,
+                return_information=True)
+
+        if save_offline:
+            tup = (bureau_offline, pi, taus, information_dict)
+            offlinefile = output_directory + 'offline_stage1.pickle'
+            swap.io.write_pickle(tup, out_path)
+            print "make_roc_curves: offline_stage1 saved to ", offlinefile
+
+        epsilon_taus = information_dict['epsilon_taus']
+        N_try = information_dict['N_try']
+
+        print "make_roc_curves: offline_stage1 processing done! epsilon, number of iterations", epsilon_taus, N_try
+
         y_true = np.array([])
         y_score = np.array([])
         for ID in training_IDs:
@@ -306,19 +232,98 @@ def make_roc_curves(argv):
                 truth = {'LENS': 1, 'NOT': 0}[subject.truth]
                 y_true = np.append(y_true, truth)
                 y_score = np.append(y_score, taus[ID])
-        fpr, tpr, threshold = roc_curve(y_true, y_score)
-        fprs.append(fpr)
-        tprs.append(tpr)
-        thresholds.append(threshold)
+        fpr_offline_stage1, tpr_offline_stage1, threshold_offline_stage1 = roc_curve(y_true, y_score)
+
+    # ------------------------------------------------------------------
+    # if we do the EM offline processing for stage2:
+    if offline_stage2:
+        print "make_roc_curves: doing offline_stage2 processing!"
+
+        # Set up data for EM algorithm
+
+        bureau_offline = {}
+        pi = 2e-4
+        taus = {}
+        online_taus = {}
+        training_IDs = {}  # which entries in collection are training
+        training_IDs_blank = {}  # we don't train on training_IDs
+        set_aside_subject = {}  # which subjects do we set aside? Here we set aside none
+        set_aside_agent = {}  # which agents do we set aside? Here we set aside none
+        n_min = 1  # minimum number of assessments required to be considered
+
+        collection = {}
+        for ID in collection2.list():
+            if ID in set_aside_subject:
+                continue
+            else:
+                collection.update({ID: collection2.member[ID]})
+        # TODO: maybe repeat the same process for bureau?
+        bureau = bureau2
+
+        for ID in collection.list():
+            subject = collection.member[ID]
+            n_assessment = len(subject.annotationhistory['ItWas'])
+            if (n_assessment > n_min):
+                if (subject.category == 'training'):
+                    truth = {'LENS': 1, 'NOT': 0}[subject.truth]
+                    # NOTE: setting this aside for here.
+                    training_IDs.update({ID: truth})
+                taus.update({ID: pi})
+                online_taus.update({ID: subject.mean_probability})
+                for agent_i in xrange(len(subject.annotationhistory['Name'])):
+                    name = subject.annotationhistory['Name'][agent_i]
+                    if name in set_aside_agent:
+                        continue
+                    xij = subject.annotationhistory['ItWas'][agent_i]
+                    if name not in bureau_offline:
+                        bureau_offline.update({name: {
+                            'Theta0': 0.75, 'Theta1': 0.75,
+                            'PL': bureau.member[name].PL,
+                            'PD': bureau.member[name].PD,
+                            'Pi': pi,
+                            'Subjects': {ID: xij}}})
+                    else:
+                        bureau_offline[name]['Subjects'].update({ID: xij})
+
+        # Run EM Algorithm
+
+        bureau_offline, pi, taus, information_dict = EM_algorithm(
+                bureau_offline, pi, taus, training_IDs_blank,
+                return_information=True)
+
+        if save_offline:
+            tup = (bureau_offline, pi, taus, information_dict)
+            offlinefile = output_directory + 'offline_stage2.pickle'
+            swap.io.write_pickle(tup, out_path)
+            print "make_roc_curves: offline_stage1 saved to ", offlinefile
+
+
+        epsilon_taus = information_dict['epsilon_taus']
+        N_try = information_dict['N_try']
+
+        print "make_roc_curves: offline_stage2 processing done! epsilon, number of iterations", epsilon_taus, N_try
+
+        y_true = np.array([])
+        y_score = np.array([])
+        for ID in training_IDs:
+            # sometimes I get nans?!
+            if taus[ID] == taus[ID]:
+                subject = collection.member[ID]
+                truth = {'LENS': 1, 'NOT': 0}[subject.truth]
+                y_true = np.append(y_true, truth)
+                y_score = np.append(y_score, taus[ID])
+        fpr_offline_stage2, tpr_offline_stage2, threshold_offline_stage2 = roc_curve(y_true, y_score)
 
     # ------------------------------------------------------------------
     # do roc curves
 
     fig, ax = plt.subplots(figsize=(10,8))
-    ax.plot(fprs[0], tprs[0], 'b', label='stage 1 online', linewidth=3)
-    ax.plot(fprs[1], tprs[1], 'DarkOrange', label='stage 2 online', linewidth=3)
-    if offline:
-        ax.plot(fprs[2], tprs[2], 'DarkOrange', linestyle='--', label='stage 2 offline', linewidth=3)
+    ax.plot(fprs[0], tprs[0], 'b', label='stage 1', linewidth=3)
+    ax.plot(fprs[1], tprs[1], 'DarkOrange', label='stage 2', linewidth=3)
+    if offline_stage1:
+        ax.plot(fpr_offline_stage1, tpr_offline_stage1, 'b', linestyle='--', label='stage 1 offline', linewidth=3)
+    if offline_stage2:
+        ax.plot(fpr_offline_stage2, tpr_offline_stage2, 'DarkOrange', linestyle='--', label='stage 2 offline', linewidth=3)
 
     ax.set_xlim(0, 0.1)
     ax.set_ylim(0.8, 1)
